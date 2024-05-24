bf7dd4f1e29c157b2ecf280ebce7fa1cf789f837
Ihor Tokar <134220707+IhorTokar@users.noreply.github.com>
2024-05-24 17:19:44 +0300

    Update 1 Завдання.sql

--- 1 Завдання.sql
+++ 1 Завдання.sql
@@ -1,7 +1,6 @@
-USE CulinaryForum;
-GO
-
--- Вибір стовпців username і email з таблиці Users
-SELECT username, email
-FROM Users;
-GO
+-- Об'єднання Users і Recipes за user_id
+SELECT user_id, username AS name
+FROM Users
+UNION
+SELECT user_id, title AS name
+FROM Recipes;
